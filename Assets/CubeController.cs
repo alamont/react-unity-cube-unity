3f79187eee7c2740291e5b3ca0e603c685515739
Andres Lamont <alamont@gmail.com>
2020-01-28 14:10:36 +0200

    Second commit!

---
+++ Assets/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(float moveAmount)
+    {
+        cube.transform.Translate(new Vector3(moveAmount, 0, 0));
+    }
+}
